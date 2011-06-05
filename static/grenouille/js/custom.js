d2e822c894b263310fc40aeed4c2a7c30dd155d4
Yohan Boniface <y.boniface@liberation.fr>
2011-06-05 12:23:43 +0200

    More mobile viewport adaptations

--- static/grenouille/js/custom.js
+++ static/grenouille/js/custom.js
@@ -6,3 +6,10 @@
 jQuery("#panel>h2").click(function () {jQuery("#categories").slideToggle()})
 // Toggle area
 jQuery("#areas>h2").click(function () {jQuery("#areas>ul").slideToggle()})
+// Auto scroll to hide the navigation bar in iPhone and iPad (Apple sucks)
+var fixSize = function () {
+    if ((/(iphone|ipod)/.test(navigator.userAgent.toLowerCase()))) {
+        window.scrollTo(0,0);
+    }
+};
+
