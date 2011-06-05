29c1e79f7dac45947f89a13cd2a2c3255828e6e6
Yohan Boniface <y.boniface@liberation.fr>
2011-06-06 00:35:24 +0200

    Cleaning the menu ; customizing the marker detail display

--- static/grenouille/js/custom.js
+++ static/grenouille/js/custom.js
@@ -6,6 +6,8 @@
 jQuery("#panel>h2").click(function () {jQuery("#categories").slideToggle()})
 // Toggle area
 jQuery("#areas>h2").click(function () {jQuery("#areas>ul").slideToggle()})
+// Toggle plus menu
+jQuery("#plus_menu>h2").click(function () {jQuery("#plus_menu>ul").slideToggle()})
 // Auto scroll to hide the navigation bar in iPhone and iPad (Apple sucks)
 var fixSize = function () {
     if ((/(iphone|ipod)/.test(navigator.userAgent.toLowerCase()))) {
